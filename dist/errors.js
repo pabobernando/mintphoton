f05e7d73bed177a5611293f1a00a20c1721f15a0
Pabo Bernando <pabobernando@desolator.local>
2025-08-12 18:16:35 +0700

    update homepage

--- dist/errors.js
+++ dist/errors.js
@@ -5,25 +5,25 @@ class MintPhotonError extends Error {
     constructor(message, code) {
         super(message);
         this.code = code;
-        this.name = 'MintPhotonError';
+        this.name = "MintPhotonError";
     }
 }
 exports.MintPhotonError = MintPhotonError;
 class WalletNotConnectedError extends MintPhotonError {
     constructor() {
-        super('Wallet is not connected', 'WALLET_NOT_CONNECTED');
+        super("Wallet is not connected", "WALLET_NOT_CONNECTED");
     }
 }
 exports.WalletNotConnectedError = WalletNotConnectedError;
 class InsufficientBalanceError extends MintPhotonError {
     constructor(required, available) {
-        super(`Insufficient balance. Required: ${required} ATONE, Available: ${available} ATONE`, 'INSUFFICIENT_BALANCE');
+        super(`Insufficient balance. Required: ${required} ATONE, Available: ${available} ATONE`, "INSUFFICIENT_BALANCE");
     }
 }
 exports.InsufficientBalanceError = InsufficientBalanceError;
 class InvalidAddressError extends MintPhotonError {
     constructor(address) {
-        super(`Invalid AtomOne address: ${address}`, 'INVALID_ADDRESS');
+        super(`Invalid AtomOne address: ${address}`, "INVALID_ADDRESS");
     }
 }
 exports.InvalidAddressError = InvalidAddressError;
